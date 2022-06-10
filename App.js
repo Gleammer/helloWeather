aac5e1d0e72e794520e9bbaa0c733fc2715513af
Gleammer <radulin4a@gmail.com>
2022-06-10 12:53:57 +0300

    Add initial axios request

--- App.js
+++ App.js
@@ -1,10 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
+import axios from 'axios';
+import React, { useEffect } from 'react'
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  useEffect(() => {
+    axios.get('https://api.weatherapi.com/v1/forecast.json?key=0837a5665aaf4ae9be293542221006&q=London&days=3&aqi=no&alerts=no')
+      .then(res => res.data)
+      .then(res => console.log(res))
+      .catch(err => console.warn(err))
+  }, [])
+
+
   return (
     <View style={styles.container}>
-      <Text>Hello, Weather!</Text>
+      <View style={styles.wrapper}>
+        <Text style={styles.wrapperTitle}>Hello, Weather!</Text>
+      </View>
     </View>
   );
 }
@@ -12,8 +24,14 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#E8EAED'
   },
+  wrapper: {
+    paddingTop: 80,
+    paddingHorizontal: 20
+  },
+  wrapperTitle: {
+    fontSize: 24,
+    fontWeight: 'bold'
+  }
 });
